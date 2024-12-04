f4399e89d2b972664795d783d19fe284a9cca627
YvesStraten <yves.straten@gmail.com>
2024-12-04 13:12:27 +0800

    WIP: preliminary saving and loading

--- src/main/java/com/yvesstraten/medicalconsolegui/MedicalGui.java
+++ src/main/java/com/yvesstraten/medicalconsolegui/MedicalGui.java
@@ -7,6 +7,11 @@ import com.yvesstraten.medicalconsole.MedicalConsole;
 
 public class MedicalGui extends JFrame {
   public static void main(String[] args) {
+    String currentOS = System.getProperty("os.name");
+    if(currentOS.equals("Mac OS X")){
+      System.setProperty("apple.laf.useScreenMenuBar", "true");
+    }
+
     HealthService service = MedicalConsole.generateSampleData();
 		MedicalGuiFrame medicalFrame = new MedicalGuiFrame(service);
 		medicalFrame.setVisible(true);
--- src/main/java/com/yvesstraten/medicalconsolegui/MedicalGuiFrame.java
+++ src/main/java/com/yvesstraten/medicalconsolegui/MedicalGuiFrame.java
@@ -1,23 +1,98 @@
 package com.yvesstraten.medicalconsolegui;
 
+import com.yvesstraten.medicalconsole.HealthService;
+import com.yvesstraten.medicalconsole.Patient;
+import com.yvesstraten.medicalconsole.facilities.MedicalFacility;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-
-import com.yvesstraten.medicalconsole.HealthService;
 
 public class MedicalGuiFrame extends JFrame {
   private HealthService service;
 
-  public MedicalGuiFrame(HealthService service){
+  public MedicalGuiFrame(HealthService service) {
     // Frame setup
-    super("HELP Medical console - currently managing " + service.getMedicalFacilities().size() + " facilities and " + service.getPatients().size() + " patients");
+    super(
+        "HELP Medical console - currently managing "
+            + service.getMedicalFacilities().size()
+            + " facilities and "
+            + service.getPatients().size()
+            + " patients");
     setSize(800, 800);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setService(service);
 
     JTabbedPane tabbedPane = new JTabbedPane();
     JPanel mainMenu = new MainMenu(service, tabbedPane);
+    JMenuBar menuBar = new JMenuBar();
+    JMenu customMenu = new JMenu("File");
+    JMenuItem load = new JMenuItem("Load");
+
+    load.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            JFileChooser fileChooser = new JFileChooser();
+            int returnVal = fileChooser.showOpenDialog(menuBar);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+              System.out.println(fileChooser.getSelectedFile().toString());
+            }
+          }
+        });
+
+    JMenuItem save = new JMenuItem("Save");
+    save.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            JFileChooser fileChooser = new JFileChooser();
+            boolean success = false;
+            while (!success) {
+            int returnVal = fileChooser.showSaveDialog(menuBar);
+              if (returnVal == JFileChooser.APPROVE_OPTION) {
+                try {
+                  FileOutputStream chosenFile = new FileOutputStream(fileChooser.getSelectedFile());
+                  ObjectOutputStream stream = new ObjectOutputStream(chosenFile);
+                  System.out.println("Writing!");
+                  for(Patient patient: service.getPatients()){
+                    System.out.println("Writing!" + patient.toString());
+                    stream.writeObject(patient);
+                  }
+
+                  for(MedicalFacility facility: service.getMedicalFacilities()){
+                    System.out.println("Writing!" + facility.toString());
+                    stream.writeObject(facility);
+                  }
+
+                  stream.close();
+
+                  success = true;
+                } catch (IOException ioe) {
+                  System.err.println(ioe.toString());
+                  returnVal = fileChooser.showSaveDialog(menuBar);
+                }
+              } else if(returnVal == JFileChooser.CANCEL_OPTION){
+                success = true;
+              }
+            }
+          }
+        });
+
+    customMenu.add(save);
+    customMenu.add(load);
+    menuBar.add(customMenu);
+
+    setJMenuBar(menuBar);
     tabbedPane.addTab("Main", mainMenu);
     add(tabbedPane);
   }
@@ -29,5 +104,4 @@ public class MedicalGuiFrame extends JFrame {
   public void setService(HealthService service) {
     this.service = service;
   }
-
 }
