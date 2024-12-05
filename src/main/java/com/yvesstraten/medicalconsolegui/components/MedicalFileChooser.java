b8283e3cef1785e11ea7fe701f5e82a3ecfd2937
YvesStraten <yves.straten@gmail.com>
2024-12-05 15:21:59 +0800

    Added reading via text

--- src/main/java/com/yvesstraten/medicalconsolegui/components/MedicalFileChooser.java
+++ src/main/java/com/yvesstraten/medicalconsolegui/components/MedicalFileChooser.java
@@ -1,195 +1,340 @@
 package com.yvesstraten.medicalconsolegui.components;
 
+import com.yvesstraten.medicalconsole.HealthService;
+import com.yvesstraten.medicalconsole.Patient;
+import com.yvesstraten.medicalconsole.facilities.Clinic;
+import com.yvesstraten.medicalconsole.facilities.Hospital;
+import com.yvesstraten.medicalconsole.facilities.MedicalFacility;
+import com.yvesstraten.medicalconsole.facilities.Procedure;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.HeadlessException;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
-import com.yvesstraten.medicalconsole.HealthService;
-import com.yvesstraten.medicalconsole.Patient;
-import com.yvesstraten.medicalconsole.facilities.MedicalFacility;
 
 public class MedicalFileChooser extends JFileChooser {
-    private SaveType saveType;
+  private SaveType saveType;
 
-    /**
-     * Default constructor
-     * Sets the save type as binary
-     * @see SaveType
-    */
-    public MedicalFileChooser(){
-        this(SaveType.BINARY);
+  /**
+   * Default constructor Sets the save type as binary
+   *
+   * @see SaveType
+   */
+  public MedicalFileChooser() {
+    this(SaveType.BINARY);
+  }
+
+  /**
+   * Alternate constructor Sets the save type to input type
+   *
+   * @param saveType type of save wanted
+   * @see SaveType
+   */
+  public MedicalFileChooser(SaveType saveType) {
+    super();
+    setSaveType(saveType);
+  }
+
+  public SaveType getSaveType() {
+    return this.saveType;
+  }
+
+  public void setSaveType(SaveType saveType) {
+    this.saveType = saveType;
+  }
+
+  /** Enum representing all possible save types */
+  public enum SaveType {
+    BINARY,
+    TEXT
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  protected JDialog createDialog(Component parent) throws HeadlessException {
+    JDialog dialog = super.createDialog(parent);
+    dialog.add(createExtraPanel(), BorderLayout.SOUTH);
+
+    dialog.pack();
+    return dialog;
+  }
+
+  private JPanel createExtraPanel() {
+    JPanel panel = new JPanel();
+    boolean selected = getSaveType() == SaveType.BINARY ? true : false;
+    String message = null;
+    if (getDialogType() == JFileChooser.SAVE_DIALOG) {
+      message = "Save as binary file";
+    } else if (getDialogType() == JFileChooser.OPEN_DIALOG) {
+      message = "Load as binary file";
     }
 
-    /**
-     * Alternate constructor
-     * Sets the save type to input type
-     * @param saveType type of save wanted
-     * @see SaveType
-    */
-    public MedicalFileChooser(SaveType saveType){
-        super();
-        setSaveType(saveType);
-    }
-
-    public SaveType getSaveType(){
-        return this.saveType;
-    }
-
-    public void setSaveType(SaveType saveType){
-        this.saveType = saveType;
-    }
-
-    /**
-     * Enum representing all possible
-     * save types
-    */
-    public enum SaveType {
-        BINARY,
-        TEXT
-    }
-
-    /**
-     * {@inheritDoc}
-    */
-    @Override
-    protected JDialog createDialog(Component parent) throws HeadlessException {
-        JDialog dialog = super.createDialog(parent);
-        dialog.add(createExtraPanel(), BorderLayout.SOUTH);
-
-        dialog.pack();
-        return dialog;
-    }
-
-    private JPanel createExtraPanel(){
-        JPanel panel = new JPanel();
-        boolean selected = getSaveType() == SaveType.BINARY ? true : false;
-        JCheckBox saveType = new JCheckBox("Save as binary file", selected);
-        saveType.addItemListener(new ItemListener() {
+    JCheckBox saveType = new JCheckBox(message, selected);
+    saveType.addItemListener(
+        new ItemListener() {
           @Override
           public void itemStateChanged(ItemEvent e) {
-            SaveType newType = getSaveType() == SaveType.BINARY
-              ? SaveType.TEXT
-              : SaveType.BINARY;
+            SaveType newType = getSaveType() == SaveType.BINARY ? SaveType.TEXT : SaveType.BINARY;
             setSaveType(newType);
-
-            System.out.println(getSaveType().toString());
           }
         });
 
-        panel.add(saveType);
-        return panel;
-    }
+    panel.add(saveType);
+    return panel;
+  }
 
-    // TODO implement
-    private void loadText(HealthService service){
-    }
+  private void loadText(HealthService service) throws IOException {
+    FileReader fr = new FileReader(getSelectedFile());
+    BufferedReader bf = new BufferedReader(fr);
+    String line = bf.readLine();
+    while (line != null) {
+      System.out.println("Current line " + line);
+      String[] split = line.split(",");
+      // Starting identifier
+      String ident = split[0];
+      if (ident.equals("PATIENT")) {
+        int id = Integer.parseInt(split[1]);
+        String name = split[2];
+        double balance = Double.parseDouble(split[3]);
+        boolean isPrivate = Boolean.parseBoolean(split[4]);
+        String facilityId = split[5];
+        MedicalFacility facility = null;
 
-    private void loadBinary(HealthService service)
-      throws IOException, ClassNotFoundException {
-          FileInputStream chosenFile = new FileInputStream(getSelectedFile());
-          ObjectInputStream inputStream = new ObjectInputStream(chosenFile);
-          Object read = inputStream.readObject();
-          while (read != null) {
-            System.out.println(read.toString());
-            if (read instanceof Patient) {
-              service.addPatient((Patient) read);
-            } else if (read instanceof MedicalFacility) {
-              service.addMedicalFacility((MedicalFacility) read);
-            }
-            read = inputStream.readObject();
+        if (!facilityId.equals(" ")) {
+          // There is a facility entry
+          int parsedId = Integer.parseInt(facilityId);
+          facility =
+              service
+                  .getMedicalFacilitiesStream()
+                  .filter(item -> service.hashCode() == parsedId)
+                  .toList()
+                  .get(0);
+        }
+
+        Patient patientToAdd = new Patient(id, name, isPrivate, balance, facility);
+
+        service.addPatient(patientToAdd);
+      } else if (ident.equals("CLINIC")) {
+        int id = Integer.parseInt(split[1]);
+        String name = split[2];
+        double fee = Double.parseDouble(split[3]);
+        double gapPercent = Double.parseDouble(split[4]);
+
+        Clinic clinic = new Clinic(id, name, fee, gapPercent);
+        service.addMedicalFacility(clinic);
+      } else if (ident.equals("HOSPITAL")) {
+        int id = Integer.parseInt(split[1]);
+        String name = split[2];
+
+        if (split[3].equals(" ")) {
+          // No procedures associated with this
+          // hospital
+          Hospital hospital = new Hospital(id, name);
+          service.addMedicalFacility(hospital);
+        } else {
+          ArrayList<Procedure> procedures = new ArrayList<Procedure>();
+
+          int i = 4;
+          System.out.println(split[split.length - 1]);
+          while (i != split.length - 1) {
+            int procedureId = Integer.parseInt(split[3]);
+            String procedureName = split[4];
+            String procedureDesc = split[5];
+            boolean isElective = Boolean.parseBoolean(split[6]);
+            double basicCost = Double.parseDouble(split[7]);
+
+            Procedure procedure =
+                new Procedure(procedureId, procedureName, procedureDesc, isElective, basicCost);
+
+            procedures.add(procedure);
+            i++;
           }
+        }
+      }
 
-        inputStream.close();
+      line = bf.readLine();
     }
 
-    // TODO Implement
-    private void saveText(HealthService service){
+    bf.close();
+  }
 
+  private void loadBinary(HealthService service) throws IOException, ClassNotFoundException {
+    FileInputStream chosenFile = new FileInputStream(getSelectedFile());
+    ObjectInputStream inputStream = new ObjectInputStream(chosenFile);
+    Object read = inputStream.readObject();
+    while (read != null) {
+      System.out.println(read.toString());
+      if (read instanceof Patient) {
+        service.addPatient((Patient) read);
+      } else if (read instanceof MedicalFacility) {
+        service.addMedicalFacility((MedicalFacility) read);
+      }
+      read = inputStream.readObject();
     }
 
-    private void saveBinary(HealthService service)
-     throws IOException {
-        FileOutputStream chosenFile = new FileOutputStream(getSelectedFile());
-        ObjectOutputStream stream = new ObjectOutputStream(chosenFile);
-        System.out.println("Writing!");
-        for (Patient patient : service.getPatients()) {
-          System.out.println("Writing!" + patient.toString());
-          stream.writeObject(patient);
+    inputStream.close();
+  }
+
+  private void saveText(HealthService service) throws IOException {
+    PrintWriter pw = new PrintWriter(getSelectedFile());
+
+    StringBuilder toWrite = new StringBuilder();
+    for (Clinic clinic : service.getClinics().toList()) {
+      toWrite
+          .append("CLINIC")
+          .append(",")
+          .append(clinic.getId())
+          .append(",")
+          .append(clinic.getName())
+          .append(",")
+          .append(clinic.getFee())
+          .append(",")
+          .append(clinic.getGapPercent())
+          .append("\n");
+    }
+
+    for (Hospital hospital : service.getHospitals().toList()) {
+      toWrite
+          .append("HOSPITAL")
+          .append(",")
+          .append(hospital.getId())
+          .append(",")
+          .append(hospital.getName())
+          .append(",");
+
+      ArrayList<Procedure> procedures = hospital.getProcedures();
+      if (procedures.size() == 0) {
+        toWrite.append(",").append(" ").append(",");
+      } else {
+        for (Procedure procedure : hospital.getProcedures()) {
+          toWrite
+              .append(procedure.getId())
+              .append(",")
+              .append(procedure.getName())
+              .append(",")
+              .append(procedure.getDescription())
+              .append(",")
+              .append(procedure.isElective())
+              .append(",")
+              .append(procedure.getCost());
         }
 
-        for (MedicalFacility facility : service.getMedicalFacilities()) {
-          System.out.println("Writing!" + facility.toString());
-          stream.writeObject(facility);
+        toWrite.append("\n");
+      }
+
+      // Last as patients depend on the facility already
+      // being loaded to load themselves
+      for (Patient patient : service.getPatients()) {
+        toWrite
+            .append("PATIENT")
+            .append(",")
+            .append(patient.getId())
+            .append(",")
+            .append(patient.getName())
+            .append(",")
+            .append(patient.getBalance())
+            .append(",")
+            .append(patient.isPrivate());
+
+        MedicalFacility current = patient.getCurrentFacility();
+        if (current == null) {
+          toWrite.append(",").append(" ").append(",");
+        } else {
+          toWrite.append(",").append(current.getId());
         }
 
-        stream.close();
+        toWrite.append("\n");
+      }
     }
 
-    public void save(HealthService service){
-        boolean success = false;
-        while (!success) {
-          int returnVal = showSaveDialog(this);
-          if (returnVal == JFileChooser.APPROVE_OPTION) {
-            try {
-                if(getSaveType() == SaveType.BINARY){
-                    saveBinary(service);
-                } else {
-                    saveText(service);
-                }
-              success = true;
-            } catch(IOException ioe){
-              // It is very likely that most IOExceptions come from the user
-              // selecting a location that they cannot save to/path to save to
-              // does not exist
-              Object[] options = new Object[] { "Ok", "Cancel" };
-              int chosenOption = JOptionPane.showOptionDialog(this,
-                                           "Could not write to file!, Please choose another location!",
-                                           "Warning",
-                                           JOptionPane.DEFAULT_OPTION,
-                                           JOptionPane.WARNING_MESSAGE,
-                                           null, options, options[0]);
+    System.out.print(toWrite.toString());
+    pw.print(toWrite.toString());
+    pw.close();
+  }
 
-              if(chosenOption == 1 &&
-                 chosenOption == JOptionPane.CANCEL_OPTION){
-                System.out.println("Abort saving");
-                success = true;
-              }
-            }
-          } else if (returnVal == JFileChooser.CANCEL_OPTION) {
+  private void saveBinary(HealthService service) throws IOException {
+    FileOutputStream chosenFile = new FileOutputStream(getSelectedFile());
+    ObjectOutputStream stream = new ObjectOutputStream(chosenFile);
+    System.out.println("Writing!");
+    for (Patient patient : service.getPatients()) {
+      System.out.println("Writing!" + patient.toString());
+      stream.writeObject(patient);
+    }
+
+    for (MedicalFacility facility : service.getMedicalFacilities()) {
+      System.out.println("Writing!" + facility.toString());
+      stream.writeObject(facility);
+    }
+
+    stream.close();
+  }
+
+  public void save(HealthService service) {
+    boolean success = false;
+    while (!success) {
+      int returnVal = showSaveDialog(this);
+      if (returnVal == JFileChooser.APPROVE_OPTION) {
+        try {
+          if (getSaveType() == SaveType.BINARY) {
+            saveBinary(service);
+          } else {
+            saveText(service);
+          }
+          success = true;
+        } catch (IOException ioe) {
+          // It is very likely that most IOExceptions come from the user
+          // selecting a location that they cannot save to/path to save to
+          // does not exist
+          Object[] options = new Object[] {"Ok", "Cancel"};
+          int chosenOption =
+              JOptionPane.showOptionDialog(
+                  this,
+                  "Could not write to file!, Please choose another location!",
+                  "Warning",
+                  JOptionPane.DEFAULT_OPTION,
+                  JOptionPane.WARNING_MESSAGE,
+                  null,
+                  options,
+                  options[0]);
+
+          if (chosenOption == 1 && chosenOption == JOptionPane.CANCEL_OPTION) {
+            System.out.println("Abort saving");
             success = true;
           }
         }
-
+      } else if (returnVal == JFileChooser.CANCEL_OPTION) {
+        success = true;
+      }
     }
+  }
 
-    public void load(HealthService service){
-        try {
-            if(getSaveType() == SaveType.BINARY){
-                loadBinary(service);
-            } else
-                loadText(service);
-        } catch (IOException ioe) {
-          JOptionPane.showMessageDialog(this,
-                                        "Error when loading file");
-        } catch (ClassNotFoundException cnfe) {
-          JOptionPane.showMessageDialog(this,
-                                        "The file has invalid data!");
-        }
+  public void load(HealthService service) {
+    try {
+      if (getSaveType() == SaveType.BINARY) {
+        loadBinary(service);
+      } else loadText(service);
+
+      // Loaded data successfully, we are done here
+      JOptionPane.showMessageDialog(this, "Data has been loaded successfully!");
+    } catch (IOException ioe) {
+      System.err.println(ioe.toString());
+      JOptionPane.showMessageDialog(this, "Error when loading file");
+    } catch (ClassNotFoundException cnfe) {
+      JOptionPane.showMessageDialog(this, "The file has invalid data!");
     }
+  }
 }
