3e32f6c789ebff260adb4cc366f0c72b19ac6057
YvesStraten <yves.straten@gmail.com>
2024-12-17 10:56:47 +0800

    Cleanup unneeded listener

--- src/main/java/com/yvesstraten/medicalconsolegui/components/ViewObjectButton.java
+++ src/main/java/com/yvesstraten/medicalconsolegui/components/ViewObjectButton.java
@@ -8,17 +8,5 @@ import javax.swing.JButton;
 public class ViewObjectButton extends JButton {
     public ViewObjectButton(){
         super("View");
-
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                System.out.println("You need to add tab adding with view!!");
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                System.out.println("You need to add tab adding with view!!");
-            }
-        });
     }
 }
